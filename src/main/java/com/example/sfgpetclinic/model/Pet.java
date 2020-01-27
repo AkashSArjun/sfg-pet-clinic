45198141b2a78521a27eadfba405f53291b1b8a7
Akash S Arjun <akashsarjun@icloud.com>
2020-01-27 11:45:55 +0100

    Adding POJO class Closes #2

---
+++ src/main/java/com/example/sfgpetclinic/model/Pet.java
@@ -0,0 +1,11 @@
+package com.example.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private LocalDate birthDate;
+    private Owner owner;
+    private PetType petType;
+
+}
